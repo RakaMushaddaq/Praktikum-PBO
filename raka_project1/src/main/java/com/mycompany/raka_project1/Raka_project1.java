6bf2384f3595084d9b31900d5c684bef351fe0e1
Raka Mushaddaq <rakamushadaq76@students.untidar.ac.id>
2026-08-29 14:39:09 +0700

    Tugas Modul 2

---
+++ raka_project1/src/main/java/com/mycompany/raka_project1/Raka_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.raka_project1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Raka_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Muhammad Raka Mushaddaq");
+        System.out.println("NPM     : 2505060072");
+        System.out.println("Alamat  : Pucang. Secang, Magelang");
+        System.out.println("No_Hp   : 087776455141");
+    }
+}
